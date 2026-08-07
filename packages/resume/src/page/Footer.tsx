f908717ffe6c1c2615b55c91004f7098a53948fd
Roman Petrov <romanpetrov@list.ru>
2026-08-07 09:42:09 +0300

    tune design

--- packages/resume/src/page/Footer.tsx
+++ packages/resume/src/page/Footer.tsx
@@ -1,15 +1,9 @@
 import { Content } from "../Content";
 import styles from "./Footer.module.scss";
-import { GithubAction } from "./GithubAction";
-import { PdfAction } from "./PdfAction";
 
 export const Footer = () => (
   <footer className={styles.root}>
     <div className={styles.inner}>
-      <div className={styles.actions}>
-        <PdfAction />
-        <GithubAction />
-      </div>
       <p className={styles.note}>{`© ${String(new Date().getFullYear())} ${Content.meta.name}`}</p>
     </div>
   </footer>
